8ff47fcef680fcd22349942e64342c9bc6ef507f
kol242 <kolinger315@gmail.com>
2022-02-25 12:19:03 +0100

    Minor design fixes

--- top-pop/src/Components/Alert.jsx
+++ top-pop/src/Components/Alert.jsx
@@ -5,7 +5,7 @@ const Alert = () => {
     <div>
         <h3 id="alert">Please enable cross-origin request by visiting <a href="https://cors-anywhere.herokuapp.com/ ">cors-anywhere.com </a>
             and start a demo server to show the app data.</h3>
-        <h4>After that, you can refresh this site.</h4> 
+        <h4 id="centered">After that, you can refresh this site.</h4> 
     </div>
   )
 }
